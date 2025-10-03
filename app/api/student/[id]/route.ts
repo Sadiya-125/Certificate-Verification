541588f23cd2de7a4472f5eb16a45dc3860798fe
Sadiya Maheen Siddiqui <wajid0257@outlook.com>
2025-10-03 19:48:31 +0530

    Initial Commit

--- app/api/student/[id]/route.ts
+++ app/api/student/[id]/route.ts
@@ -1,24 +1,29 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
-    const studentParams = await params;
-    const studentId = studentParams.id;
+    const studentId = params.id;
     const student = await prisma.student.findUnique({
-      where: { id: studentId }
+      where: { id: studentId },
     });
-    
+
     if (!student) {
-      return NextResponse.json({ message: 'Student Not Found' }, { status: 404 });
+      return NextResponse.json(
+        { message: "Student Not Found" },
+        { status: 404 }
+      );
     }
-    
+
     return NextResponse.json(student);
   } catch (error) {
-    console.error('Error Fetching Student:', error);
-    return NextResponse.json({ message: 'An Error Occurred While Fetching Student Data' }, { status: 500 });
+    console.error("Error Fetching Student:", error);
+    return NextResponse.json(
+      { message: "An Error Occurred While Fetching Student Data" },
+      { status: 500 }
+    );
   }
 }
